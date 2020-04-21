a2418b6d0fc11f286eb198751a9c51afb3b1be4d
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-21 16:40:20 +0200

    add(login/register): basic components and logic

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="l-stack-large">
+    <header class="header l-ptb3 box">
+      <div class="l-container flex">
+        <h1 class="h3">Accueil</h1>
+      </div>
+    </header>
+
+    <div class="l-stack-separated">
+      <stats />
+
+      <main class="l-container">
+        <graph />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Stats from '~/components/stats/Stats'
+import Graph from '~/components/stats/Graph'
+
+export default {
+  components: {
+    Stats, Graph
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="login">
+    <input v-model="email" type="text" placeholder="Your email" />
+    <input v-model="password" type="password" placeholder="Your password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<script>
+import { login } from '@/services/login'
+
+export default {
+  methods: {
+    async login() {
+      const user = await login(this.email, this.password);
+      console.log('login:', {user});
+    }
+  },
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
