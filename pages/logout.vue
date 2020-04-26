5a25139d4bc0825c8df7eda1770a43f21e75ac63
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-26 14:54:19 +0200

    add(logout page)

---
+++ pages/logout.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>À bientôt!</p>
+</template>
+
+<script>
+import { logout } from '@/services/firebase-logout';
+
+export default {
+  methods: {
+    async logout() {
+      const result = await logout();
+    }
+  }
+}
+</script>
