c182fc7c8245566f828eeaa97ec71503695307f6
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-27 09:20:20 +0200

    wip feat(login): using new form components

--- pages/login.vue
+++ pages/login.vue
@@ -1,34 +1,72 @@
 <template>
-  <form @submit.prevent="login">
-    <input v-model="email" type="text" placeholder="Your email" />
-    <input v-model="password" type="password" placeholder="Your password" />
-    <button type="submit">Login</button>
-  </form>
+  <app-form
+    class="auth__panel auth__panel--primary"
+    :use-csrf="false"
+    :store="store"
+    :processForm="handleLogin"
+    :handleProcessFormError="handleLoginError"
+    :successCallback="handleSuccess"
+  >
+
+    <div slot="form-body">
+      <h3>Se connecter</h3>
+      <form-input name="email" type="email" label="E-mail" />
+      <form-input name="password" type="password" label="Mot de passe" />
+    </div>
+
+    <form-submit class="l-mt2" slot="form-actions">Se connecter</form-submit>
+  </app-form>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { login, LoginResponse } from '@/services/login'
-import {AuthService, AuthServiceInterface} from '@/components/auth/auth'
+import { login, LoginResponse } from '@/services/login';
+import {AuthService, AuthServiceInterface} from '@/components/auth/auth';
+import FormStore from '@/components/form/FormStore';
+import FormInput from '@/components/form/Input.vue';
+import FormSubmit from '@/components/form/Submit.vue';
+import AppForm, {ErrorsInterface} from '@/components/form/Form.vue';
+import { auth } from '@/services/firebase-init';
+
+
+interface LoginStore {
+  email: string,
+  password: string
+};
+
+const formStore = new FormStore<LoginStore>({
+  email: '',
+  password: ''
+});
+
+async function handleLogin({email, password}: LoginStore) {
+  return await auth.signInWithEmailAndPassword(email, password);
+};
+
+function handleLoginError(e: any): ErrorsInterface {
+  console.log({e});
+  return {
+    'email': ['Bad email!']
+  }
+};
 
 export default Vue.extend({
+  components: {
+    FormInput, FormSubmit, AppForm
+  },
   methods: {
-    async login() {
-      const loginResponse: LoginResponse = await login(this.email, this.password);
-      if (loginResponse.success === true) {
-        this.authService.login(loginResponse.user);
-        this.$router.push('/');
-      } else {
-        window.alert('Login failed');
-      }
+    handleSuccess(user: Object) {
+      this.authService.login(user);
+      this.$router.push('/');
     }
   },
   data() {
     return {
       authService: new AuthService(this.$store),
-      email: '',
-      password: ''
+      store: formStore,
+      handleLogin,
+      handleLoginError
     }
   }
-})
+});
 </script>
