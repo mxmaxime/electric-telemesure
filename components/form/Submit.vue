dcf0199427a509e20bf2ad85ea244d280e3a6e6b
Maxime Moreau <contact@maxime-moreau.fr>
2020-04-28 11:35:52 +0200

    refactor(extract submit style)

--- components/form/Submit.vue
+++ components/form/Submit.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="l-btn"
+    class="btn"
     type="submit"
     >
     <slot></slot>
@@ -12,83 +12,7 @@
   </button>
 </template>
 
-<style lang="scss" scoped>
-.loader,
-.loader:before,
-.loader:after {
-  background: #fff;
-  animation: load1 1s infinite ease-in-out;
-  height: 22px;
-  width: 0.2em;
-  transform: scaleY(0.5);
-}
-
-.loader-wrapper {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: #855cc7;
-  padding: 12px 24px;
-}
-
-.loader {
-  position: relative;
-  color: #fff;
-  text-indent: -9999em;
-  margin: auto;
-  position: relative;
-  transform: translateZ(0);
-  animation-delay: -0.16s;
-}
-
-.loader:before,
-.loader:after {
-  position: absolute;
-  top: 0;
-  content: '';
-}
-
-.loader:before {
-  left: -8px;
-  animation-delay: -0.32s;
-}
-
-.loader:after {
-  left: 8px;
-}
-
-@keyframes load1 {
-  0%,
-  80%,
-  100% {
-    transform: scaleY(0.5);
-  }
-  40% {
-    transform: scaleY(1);
-  }
-}
-
-$loader: .25s;
-
-.loader-leave-active, .loader-enter-active {
-  transition: transform $loader;
-}
-
-.loader-enter-active {
-  transition-timing-function: cubic-bezier(0.0, 0.0, 0.2, 1)
-}
-
-.loader-leave-active {
-  transition-timing-function: cubic-bezier(0.4, 0.0, 1, 1)
-}
-
-.loader-enter, .loader-leave-to {
-  transform: translateY(100%);
-}
-
-</style>
+<style lang="scss" scoped src="./submit.scss"></style>
 
 <script>
 export default {
