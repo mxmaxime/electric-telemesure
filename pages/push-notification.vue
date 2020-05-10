bbe1c9b12c20ea55d6356ca40763a641ac222579
Maxime Moreau <contact@maxime-moreau.fr>
2020-05-10 14:06:32 +0200

    wip(firebase cloud messaging): doesn't work, service worker's missing.

---
+++ pages/push-notification.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="askPermission">Enable push notification</button>
+</template>
+
+<script lang="ts">
+
+import { askPermission } from '@/services/push-notifications'
+
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    askPermission() {
+      askPermission().then((result) => {
+        console.log({result});
+      }).catch(e => console.error)
+    }
+  },
+
+})
+</script>
